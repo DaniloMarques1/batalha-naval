6a6d2edc362e1f2c17393555287fd401091c0e4a
Danilo Marques <danilomarques20@hotmail.com>
2019-02-11 20:46:20 -0300

    Gerando visualizaão da matriz

---
+++ main.py
@@ -0,0 +1,41 @@
+letters = {"A":0, "B":1, "C":2,"D":3,"E":4}
+
+
+def gerarTabuleiro():
+	'''
+	gera os tabuleiros da batalha
+	'''
+	import random
+	t1 = []
+	for i in range(10):
+		linha = []
+		for j in range(10):
+			linha.append(random.randint(0,4))
+		t1.append(linha)
+	return t1
+
+
+t1 = gerarTabuleiro();
+
+def exibirTabuleiros(tabuleiro):
+	'''
+	Gera uma visualização do tabuleiro
+	'''
+	print(" ", end="")
+	print(" A", end="")
+	print(" B", end="")
+	print(" C", end="")
+	print(" D", end="")
+	print(" E", end="")
+	print(" F", end="")
+	print(" G", end="")
+	print(" H", end="")
+	print(" I", end="")
+	print(" J")
+	for i in range(len(tabuleiro)):
+		print(i,end="")
+		for j in range(len(tabuleiro[0])):
+			print(" x",end="")
+		print()	
+
+exibirTabuleiros(t1)	
