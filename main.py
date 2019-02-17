16ddbb98e2939c4aa96a419df87a7b42832676af
Danilo Marques <danilomarques20@hotmail.com>
2019-02-17 16:16:19 -0300

    Geração dos tabuleiros/navios

--- main.py
+++ main.py
@@ -1,50 +1,423 @@
-letters = {"A":0, "B":1, "C":2,"D":3,"E":4}
+#Constantes
+letters = {"A":0, "B":1, "C":2,"D":3,"E":4, "F":5, "G":6,"H":7,"I":8,"J":9}
+#primeiro player
+celulas1 = []
+tabuleiro1 = []
+submarinos1 = []
+cruzadores1 = []
+porta_avioes1 = []
 
-def celulasDisponiveis():
-	m = []
-	for i in range(10):
-		for j in range(10):
-			m.append([i,j])
-	return m
+#segundo jogador
+celulas2 = []
+tabuleiro2 = []
+submarinos2 = []
+cruzadores2 = []
+porta_avioes2 = []
+def main():
+	#JOGADORES
+	player1 = input("Nome do primeiro jogador: ")
+	print("#" * 10)
+	print("Gerando tabuleiros")
+	print("#" * 10)
+	#gerando submarinos primeiro player
+	celulasDisponiveis(celulas1, tabuleiro1)
+	gerarTabuleiro(celulas1, tabuleiro1, submarinos1, cruzadores1, porta_avioes1)		
+	exibirTabuleiros(tabuleiro1)
+	#pegando coordenadas (tiros) do usuario
+	play(tabuleiro1, submarinos1, cruzadores1, porta_avioes1)
+def play(tabuleiro, submarinos, cruzadores, porta_avioes):
+	while True:
+		linha = letters[input("Linha: ")]
+		coluna = int(input("Coluna: ")) - 1
+		coordenada = [linha, coluna]
+		print("Coordenada: ", coordenada)
+		if not coordenada:
+			break
 
-celulas = celulasDisponiveis()
-print(celulas)
+		if hitASubmarino(coordenada, submarinos, tabuleiro):
+			print("HIT SUBMARINO")
+		elif hitACruzador(coordenada, cruzadores, tabuleiro):
+			print("HIT CRUZADOR")
+		elif hitPortaAviao(coordenada, porta_avioes, tabuleiro):
+			print("HIT PORTA AVIAO")	
 
-def gerarTabuleiro():
+		exibirTabuleiros(tabuleiro)
+
+
+def celulasDisponiveis(celulas,tabuleiro):
 	'''
-	gera os tabuleiros da batalha
-	'''
-	import random
-	t1 = []
+	gera todas as celulas disponiveis
+	'''	
 	for i in range(10):
 		linha = []
 		for j in range(10):
-			linha.append(random.randint(0,4))
-		t1.append(linha)
-	return t1
+			celulas.append([i,j])
+			linha.append("-")
+		tabuleiro.append(linha)
+		print("LEN TABULEIRO: ", len(tabuleiro))
 
+def hitASubmarino(coordenada, submarinos, tabuleiro):
+	'''
+	coordenada: coordenada dada pelo usuario
+	submarinos: matriz dos submarinos gerados aleatoriamente
+	tabuleiro: tabuleiro do game
+	'''
+	#Submarino
+	for i in submarinos:
+		if coordenada in i:
+			linha, coluna = coordenada[0], coordenada[1] 
+			tabuleiro[linha][coluna] = "S"
+			i.remove(coordenada)
+			if len(i) == 0:
+				print("Destruiu submarino")
+			return True
+	return False
+				
+def hitACruzador(coordenada, cruzadores, tabuleiro):
+	'''
+	coordenada: coordenada dada pelo usuario
+	cruzadores: matriz dos cruzadores gerados aleatoriamente
+	tabuleiro: tabuleiro do game
+	'''
+	for i in cruzadores:
+		if coordenada in i:
+			linha, coluna = coordenada[0], coordenada[1]
+			tabuleiro[linha][coluna] = "C"
+			i.remove(coordenada)
+			if len(i) == 0:
+				print("Destruiu cruzador")
+			return True	
+	return False
 
-t1 = gerarTabuleiro();
+def hitPortaAviao(coordenada, porta_avioes, tabuleiro):
+	'''
+	coordenada: coordenada dada pelo usuario
+	porta_avioes: matriz dos portas avioes gerados aleatoriamente
+	tabuleiro: tabuleiro do game
+	'''
+	for i in porta_avioes:
+		if coordenada in i:
+			linha,coluna = coordenada[0], coordenada[1]
+			tabuleiro[linha][coluna] = "P"
+			i.remove(coordenada)
+			if len(i) == 0:
+				print("Destruiu porta aviao")
+			return True
+	return False			
+
+def gerarTabuleiro(celulas, tabuleiro, submarinos, cruzadores, porta_avioes):
+	'''
+	celulas: celulas disponiveis para gerar os navios
+	tabuleiro: as posições do jogo
+	submarinos: a matriz que sera preenchida com as coordenadas dos submarino(s)
+	cruzadores: a matriz que sera preenchida com as coordenadas dos cruzador(es)
+	porta_avioes: a matriz que sera preenchida com as coordenadas dos porta_avioe(s)
+	'''
+	sQtd = int(input("Quantos submarinos deseja? "))
+	for i in range(sQtd):
+		while True:
+			s1 = submarino(celulas)
+			if s1 != -1:
+				submarinos.append(s1)
+				break
+			#caso tenha removido alguma celula mas não tenha sido possivel formar um navio, resetar as celulas disponiveis
+			celulasDisponiveis(celulas)
+	print(submarinos)	
+	#gerando os cruzadores
+	cQtd = int(input("Quantos cruzadores: "))
+	for i in range(cQtd):
+		while True:
+			c1 = cruzador(celulas)
+			if c1 != -1:
+				cruzadores.append(c1)
+				break
+			#caso tenha removido alguma celula mas não tenha sido possivel formar um navio, resetar as celulas disponiveis	
+			celulasDisponiveis(celulas, tabuleiro)
+	print(cruzadores)
+
+	#gerando os porta avioes 
+	paQtd = int(input("Qunatos porta avioes: "))
+	for i in range(paQtd):
+		while True:
+			pa = porta_aviao(celulas)
+			if pa != -1:
+				porta_avioes.append(pa)
+				break
+			#caso tenha removido alguma celula mas não tenha sido possivel formar um navio, resetar as celulas disponiveis	
+			celulasDisponiveis(celulas, tabuleiro)
+	print(porta_avioes)
 
 def exibirTabuleiros(tabuleiro):
 	'''
 	Gera uma visualização do tabuleiro
 	'''
-	print(" ", end="")
-	print(" A", end="")
-	print(" B", end="")
-	print(" C", end="")
-	print(" D", end="")
-	print(" E", end="")
-	print(" F", end="")
-	print(" G", end="")
-	print(" H", end="")
-	print(" I", end="")
-	print(" J")
-	for i in range(len(tabuleiro)):
-		print(i,end="")
-		for j in range(len(tabuleiro[0])):
-			print(" x",end="")
-		print()	
+	print("#" * 12)
+	print("* ", end="")
+	for i in range(1, 11):
+		print(f"{i}  ",end="")
+	print()
+	letter = 65	
+	for i in range(10):
+		print(f"{chr(letter)} ",end="")
+		letter += 1
+		for j in range(10):			
+			print(f'{tabuleiro[i][j]:3}', end="")
+		print()
+	print("#" * 12)	
+def direita(celulas,position, n):
+	'''
+	celulas: celulas disponiveis para geração do navio
+	position: uma possivel posição para o navio
+	n: quantas casas, a partir da primeira pular para a direita
+	'''
+	newPosition = [position[0], position[1] + n]
+	if isAvailable(celulas,newPosition):
+		return newPosition
 
-exibirTabuleiros(t1)	
+def esquerda(celulas,position, n):
+	'''
+	celulas: celulas disponiveis para geração do navio
+	position: uma possivel posição para o navio
+	n: quantas casas, a partir da primeira pular para a esquerda
+	'''
+	newPosition = [position[0], position[1] - n]
+	if isAvailable(celulas,newPosition):
+		return newPosition
+
+def up(celulas,position, n):
+	'''
+	celulas: celulas disponiveis para geração do navio
+	position: uma possivel posição para o navio
+	n: quantas casas, a partir da primeira pular para cima
+	'''
+	newPosition = [position[1] - n, position[1]]
+	if isAvailable(celulas,newPosition):
+		return newPosition
+
+def down(celulas,position, n):
+	'''
+	celulas: celulas disponiveis para geração do navio
+	position: uma possivel posição para o navio
+	n: quantas casas, a partir da primeira pular para a baixo
+	'''
+	newPosition = [position[1] + n, position[1]]
+	if isAvailable(celulas,newPosition):
+		return newPosition
+
+
+def submarino(celulas):
+	'''
+	celulas: celulas disponveis para geração do submarino
+	retorna: uma matriz com as coordenadas do submarino gerado
+	'''
+	import random
+	cel = []
+	index = random.randint(0, len(celulas) - 1)
+	firstPosition = celulas[index]
+	celulas.remove(firstPosition)
+	vertical = 0
+	newPosition = direita(celulas,firstPosition, 1)
+	if newPosition:
+		cel.append(newPosition)
+	else:
+		newPosition = esquerda(celulas,firstPosition, 1)
+		if newPosition:
+			cel.append(newPosition)
+		else:
+			vertical = 1
+
+	if vertical == 1:
+		newPosition = down(celulas,firstPosition, 1)
+		if newPosition:
+			cel.append(newPosition)
+		else:
+			newPosition = up(firstPosition, 1)
+			if newPosition:
+				cel.append(newPosition)	
+			else:
+				return -1
+
+				
+	cel.append(firstPosition)
+	return cel
+
+def cruzador(celulas):
+	'''
+	celulas: celulas disponveis para geração do cruzador
+	retorna: uma matriz com as coordenadas do cruzador gerado
+	'''
+	import random
+	cel = []
+	index = random.randint(0, len(celulas) - 1)
+	firstPosition = celulas[index]
+	celulas.remove(firstPosition)
+	horizontal = 0
+	vertical = 0
+	newPosition1 = direita(celulas,firstPosition, 1)
+	cel.append(firstPosition)
+
+	#testa posições a direita
+	if newPosition1:	
+		newPosition2 = direita(celulas,firstPosition, 2)	
+		if newPosition2:
+			cel.append(newPosition1)
+			cel.append(newPosition2)
+		else:
+			horizontal = 1
+	else:
+		horizontal = 1		
+ 	
+	#testa posições a esquerda
+	if horizontal == 1:
+ 		newPosition1 = esquerda(celulas,firstPosition, 1)
+ 		if newPosition1:
+ 			newPosition2 = esquerda(celulas,firstPosition, 2)
+ 			if newPosition2:
+ 				cel.append(newPosition1)
+ 				cel.append(newPosition2)
+ 			else:
+ 				vertical = 1	
+ 		else:
+ 			vertical = 1
+
+ 	#testa posições para baixo		
+	if vertical == 1:
+ 		newPosition1 = down(celulas,firstPosition, 1)
+ 		if newPosition1:
+ 			newPosition2 = down(celulas,firstPosition, 2)
+ 			if newPosition2:
+ 				cel.append(newPosition1)
+ 				cel.append(newPosition2)
+ 			else:
+ 				vertical = 2	
+ 		else:
+ 			vertical = 2
+	#Testa posições para cima
+	if vertical == 2:
+		newPosition1 = up(celulas,firstPosition, 1)
+		if newPosition1:	
+			newPosition2 = up(celulas,firstPosition, 2)
+			if newPosition2:
+				cel.append(newPosition1)
+				cel.append(newPosition2)
+			else:
+				return -1
+		else:
+			return -1			
+				
+	return cel
+
+def porta_aviao(celulas):
+	'''
+	celulas: celulas disponiveis para geração do porta avião
+	preenche uma matriz com as coordenadas do porta avião e a retorna
+	'''
+	import random
+	cel = []
+	index = random.randint(0, len(celulas) - 1)
+	firstPosition = celulas[index]
+	celulas.remove(firstPosition)
+	horizontal = 0
+	vertical = 0
+	newPosition1 = direita(celulas,firstPosition, 1)
+	if newPosition1:
+		newPosition2 = direita(celulas,firstPosition, 2)
+		if newPosition2:
+			newPosition3 = direita(celulas,firstPosition, 3)
+			if newPosition3:
+				cel.append(newPosition1)
+				cel.append(newPosition2)
+				cel.append(newPosition3)
+			else:
+				horizontal = 1	
+		else:
+			horizontal = 1
+	else:
+		horizontal = 1		
+ 	
+	if horizontal == 1:
+		newPosition1 = esquerda(celulas,firstPosition, 1)
+		if newPosition1:
+			newPosition2 = esquerda(celulas,firstPosition, 2)
+			if newPosition2:
+				newPosition3 = esquerda(celulas,firstPosition, 3)
+				if newPosition3:
+					cel.append(newPosition1)
+					cel.append(newPosition2)
+					cel.append(newPosition3)
+				else:
+					vertical = 1	
+			else:
+				vertical = 1	
+		else:
+			vertical = 1
+	
+
+	if vertical == 1:
+		newPosition1 = down(celulas,firstPosition, 1)
+		if newPosition1:
+			newPosition2 = down(celulas,firstPosition, 2)
+			if newPosition2:
+				newPosition3 = down(celulas,firstPosition, 3)
+				if newPosition3:
+					cel.append(newPosition1)
+					cel.append(newPosition2)
+					cel.append(newPosition3)
+				else:
+					vertical = 2		
+			else:
+				vertical = 2
+		else:
+			vertical = 2
+				
+
+	if vertical == 2:
+		newPosition1 = up(celulas,firstPosition, 1)
+		
+		if newPosition1:
+			newPosition2 = up(celulas,firstPosition, 2)
+			if newPosition2:
+				newPosition3 = up(celulas,firstPosition, 3)
+				if newPosition3:
+					cel.append(newPosition1)
+					cel.append(newPosition2)
+					cel.append(newPosition3)
+				else:
+					return -1
+			else:
+				return -1
+		else:
+			return -1
+								
+
+				
+	cel.append(firstPosition)
+	return cel	
+
+def isAvailable(celulas, newPosition):
+	'''
+	celulas: celulas disponiveis para criação de um navio
+	newPosition: uma nova posição
+	retorna true/false dependendo de sua disponibilidade
+	'''
+	if newPosition in celulas:		
+		celulas.remove(newPosition)
+		return True
+	return False
+	
+main()
+
+
+
+
+
+# print(n1)
+# while True:
+# 	c1 = cruzador()
+# 	if c1 != -1:
+# 		break
+# while True:
+# 	pa1 = porta_aviao()
+# 	if pa1 != -1:
+# 		break
+
